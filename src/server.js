acea0d5519879ee3772301222d1b66b212df3522
Farma27 <farmafnr12@gmail.com>
2024-11-19 13:05:23 +0700

    Add initial server setup and configuration with Hapi framework

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi');
+// const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    }
+  });
+
+  // server.routes(routes);
+
+  // await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+init();

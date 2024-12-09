7ef8b219364aeb497d68e69b5b6978024737802b
Farma27 <farmafnr12@gmail.com>
2024-12-09 09:12:30 +0000

    Add root route with a simple HTML response

---
+++ src/routes/rootRoutes.js
@@ -0,0 +1,33 @@
+const rootRoutes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return `
+        <html>
+          <head>
+            <title>Nothing to see here</title>
+            <style>
+              body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+                font-family: Arial, sans-serif;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>Nothing to see here</h1>
+          </body>
+        </html>
+      `;
+    },
+    options: {
+      auth: false // No authentication required for this route
+    }
+  }
+];
+
+module.exports = rootRoutes;
